d5918492af36f390c8286feb7441dff784f2a0fa
lalucia2k20-hue <lalucia2k20@gmail.com>
2026-06-22 10:07:53 -0300

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,12 @@
+[9:37 a. m., 16/6/2026] Lucia Poblete: document.getElementById("formulario")
+.addEventListener("submit", function() {
+
+alert("Formulario enviado correctamente");
+
+});
+[9:38 a. m., 16/6/2026] Lucia Poblete: document.getElementById("formulario")
+.addEventListener("submit", function() {
+
+alert("Formulario enviado correctamente");
+
+});
